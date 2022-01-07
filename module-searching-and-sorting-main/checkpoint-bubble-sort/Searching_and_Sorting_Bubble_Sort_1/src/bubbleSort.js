b64bff4836cd27b58aa3823c0cd433227a716118
Dakota Watkins <dakota.watkins007@gmail.com>
2022-01-07 08:11:27 -0800

    updated bubble sort

--- module-searching-and-sorting-main/checkpoint-bubble-sort/Searching_and_Sorting_Bubble_Sort_1/src/bubbleSort.js
+++ module-searching-and-sorting-main/checkpoint-bubble-sort/Searching_and_Sorting_Bubble_Sort_1/src/bubbleSort.js
@@ -3,20 +3,20 @@ function bubbleSort(compare, elements) {
     let inOrder;
 
     do {
-      inOrder = true;
+      inOrder = true; // Assume that the array is in order
 
       for (
         let index = 0, length = elements.length - 1;
-        index < elements.length;
+        index < length;
         index++
       ) {
         const leftElement = elements[index];
         const rightElement = elements[index + 1];
 
         if (compare(leftElement, rightElement) > 0) {
-          elements[index + 1] = leftElement;
           elements[index] = rightElement;
-          inOrder = false;
+          elements[index + 1] = leftElement;
+          inOrder = false; // The array wasn't in order, so swap elements and then check it again.
         }
       }
     } while (inOrder === false);
